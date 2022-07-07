219ecc66b4a183ebecbd53321ff7416cfae00239
bder00s <bonne-deroos@hotmail.com>
2022-07-07 18:48:42 +0200

    dist files toegevoegd

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 console.log('Hallo daar!');
+
+import axios from "axios";
+
+//Stap 1. Welk endpoint heb je nodig? https://restcountries.com/v2/all
+
+//Stap 2. Schrijf een asynchrone functie die, met behulp van Axios, een GET-request maakt naar het juiste endpoint. Log de response in de console en bestudeer de data goed: hoe is het opgebouwd?
+
+
+
+
