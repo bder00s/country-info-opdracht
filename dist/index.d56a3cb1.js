0a7f7183f2ec184b9946f8646fb5413bed4b20c0
bder00s <bonne-deroos@hotmail.com>
2022-07-08 10:16:35 +0200

    huiswerkklas 1

--- dist/index.d56a3cb1.js
+++ dist/index.d56a3cb1.js
@@ -465,14 +465,20 @@ var _axiosDefault = parcelHelpers.interopDefault(_axios);
 console.log('Hallo daar!');
 //Stap 1. Welk endpoint heb je nodig? https://restcountries.com/v2/all
 //Stap 2. Schrijf een asynchrone functie die, met behulp van Axios, een GET-request maakt naar het juiste endpoint. Log de response in de console en bestudeer de data goed: hoe is het opgebouwd?
-async function fetchData() {
+//Stap 3. Zet de referenties naar de HTML Data in het Javascript bestand
+const countryList = document.getElementById('list-of-countries');
+async function fetchCountryInfo() {
     try {
-        const result = await _axiosDefault.default.get('https://restcountries.eu/rest/v2/all');
-        console.log(result);
-    } catch (e) {
-        console.error(e);
+        const response = await _axiosDefault.default.get('https://restcountries.com/v2/all');
+        console.log(response.data); //Dit is de request
+        //Hier de data injecteren!
+        countryList.innerHTML = ` <li>in het land ${response.data[0].name} spreken ze de taal ${response.data[0].languages[0].name}</li> `;
+    } catch (error) {
+        console.error(error); //Dit is de error catch
+    //Hier de errormelding injecteren in browser
     }
 }
+fetchCountryInfo();
 
 },{"axios":"1IeuP","@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}],"1IeuP":[function(require,module,exports) {
 module.exports = require('./lib/axios');
--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import axios from "axios";
 
 //Stap 2. Schrijf een asynchrone functie die, met behulp van Axios, een GET-request maakt naar het juiste endpoint. Log de response in de console en bestudeer de data goed: hoe is het opgebouwd?
 
+//Stap 3. Zet de referenties naar de HTML Data in het Javascript bestand
+
+const countryList = document.getElementById('list-of-countries');
+
+async function fetchCountryInfo() {
+    try {
+        const response = await axios.get('https://restcountries.com/v2/all');
+        console.log(response.data); //Dit is de request
+
+        //Hier de data injecteren!
+        countryList.innerHTML = ` <li>in het land ${response.data[0].name} spreken ze de taal ${response.data[0].languages[0].name}</li> `
 
 
 
+    } catch (error) {
+        console.error(error); //Dit is de error catch
+
+        //Hier de errormelding injecteren in browser
+
+    }
+}
+fetchCountryInfo();
