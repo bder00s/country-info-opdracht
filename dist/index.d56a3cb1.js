465d3c54bb5d5327ab437b166032f78570b0b562
bder00s <bonne-deroos@hotmail.com>
2022-07-08 11:48:38 +0200

    opdracht verder afgemaakt in huiswerkklas

--- dist/index.d56a3cb1.js
+++ dist/index.d56a3cb1.js
@@ -464,21 +464,33 @@ var _axios = require("axios");
 var _axiosDefault = parcelHelpers.interopDefault(_axios);
 console.log('Hallo daar!');
 //Stap 1. Welk endpoint heb je nodig? https://restcountries.com/v2/all
-//Stap 2. Schrijf een asynchrone functie die, met behulp van Axios, een GET-request maakt naar het juiste endpoint. Log de response in de console en bestudeer de data goed: hoe is het opgebouwd?
+//Stap 2. Schrijf een asynchrone functie die (met try + catch), met behulp van Axios, een GET-request maakt naar het juiste endpoint. Log de response in de console en bestudeer de data goed: hoe is het opgebouwd?
 //Stap 3. Zet de referenties naar de HTML Data in het Javascript bestand
-const countryList = document.getElementById('list-of-countries');
+const countryData = document.getElementById('country');
 async function fetchCountryInfo() {
     try {
         const response = await _axiosDefault.default.get('https://restcountries.com/v2/all');
         console.log(response.data); //Dit is de request
-        //Hier de data injecteren!
-        countryList.innerHTML = ` <li>in het land ${response.data[0].name} spreken ze de taal ${response.data[0].languages[0].name}</li> `;
+        allCountries(response.data);
+    //Hier de data injecteren!
     } catch (error) {
         console.error(error); //Dit is de error catch
     //Hier de errormelding injecteren in browser
     }
 }
 fetchCountryInfo();
+function allCountries(countryList) {
+    const countryData = document.getElementById('country');
+    countryData.innerHTML = countryList.map((countries)=>{
+        return `<li>
+<h3>${countries.name}</h3> 
+ <img src="${countries.flag}" alt="plaatje van de Afghaanse vlag"/>
+ <p>Has a population of: ${countries.population} people</p>
+ </li>
+`;
+    });
+}
+allCountries(countries);
 
 },{"axios":"1IeuP","@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}],"1IeuP":[function(require,module,exports) {
 module.exports = require('./lib/axios');
